931fc075858a0bc1387bc059e0241d7039857765
nithyatsu <nithyasu@microsoft.com>
2025-09-08 11:13:43 -0700

    rename folder and add recipes
    Signed-off-by: nithyatsu <nithyasu@microsoft.com>

---
+++ Compute/persistantVolumes/recipes/kubernetes/terraform/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0"
+    }
+  }
+}
+locals {
+  uniqueName = var.context.resource.name
+  namespace = var.context.runtime.kubernetes.namespace
+}
+
+resource "kubernetes_persistent_volume" "pv" {
+  metadata {
+    name = var.context.resource.name
+    labels = {
+      resource = var.context.resource.name
+      # Label pods with the application name so `rad run` can find the logs.
+      "radapp.io/application" = var.context.application != null ? var.context.application.name : ""
+    }
+  }
+
+  spec {
+    storage_class_name = var.storage_class
+
+    capacity = {
+      storage = var.context.resource.properties.sizeInGib
+    }
+
+    access_modes = can(var.context.resource.properties.allowedAccessModes) ? [var.context.resource.properties.allowedAccessModes] : null
+
+    persistent_volume_source {
+      host_path {
+        path = var.host_path
+        type = "DirectoryOrCreate"
+      }
+    }
+  }
+}
+
+output "result" {
+  value = {
+    resources = [
+      "/planes/kubernetes/local/providers/core/PersistentVolume/${var.context.resource.name}"
+    ]
+  }
+}
