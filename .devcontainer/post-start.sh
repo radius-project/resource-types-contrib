97dc7c234c87859c2990e4ed2d07f109e02d8d9f
SoTrx <11771975+SoTrx@users.noreply.github.com>
2025-11-15 11:10:08 +0100

    chore : Disbaling post-start proxy command and rewording comment
    Signed-off-by: SoTrx <11771975+SoTrx@users.noreply.github.com>

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 set -eo pipefail
 
-# Note: Using docker outside of docker means the oras server will run on the host docker daemon.
-# Thus the "localhost:5000" used in the build scripts will not work inside the devcontainer, it should be <host-ip>:5000.
-# We can use host.docker.internal to access the host from inside the container. We'll proxy the port using socat.
-exec socat TCP-LISTEN:5000,reuseaddr,fork TCP:host.docker.internal:5000
+# Note: Uncomment the following line if you get "Connection refused" on localhost:5000 while building recipes
+# exec socat TCP-LISTEN:5000,reuseaddr,fork TCP:host.docker.internal:5000
