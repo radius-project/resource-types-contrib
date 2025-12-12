7d327e6a59eeb07093b353aa26a98c608b47fc90
nithyatsu <nithyasu@microsoft.com>
2025-12-12 14:02:59 -0800

    wip
    Signed-off-by: nithyatsu <nithyasu@microsoft.com>

--- .github/scripts/test-recipe.sh
+++ .github/scripts/test-recipe.sh
@@ -75,6 +75,7 @@ echo "==> Deploying test application from $TEST_FILE"
 APP_NAME="testapp-$(date +%s)"
 
 # Deploy the test app
+echo "going to deploy app: $APP_NAME"
 if rad deploy "$TEST_FILE" --application "$APP_NAME" --environment /planes/radius/local/resourceGroups/default/providers/Radius.Core/environments/default; then
     echo "==> Test deployment successful"
     
