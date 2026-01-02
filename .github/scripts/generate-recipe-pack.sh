3e59c3cbe39791ad60bdcf0825fe4e416308d63c
nithyatsu <nithyasu@microsoft.com>
2026-01-02 15:28:17 -0800

    wip
    Signed-off-by: nithyatsu <nithyasu@microsoft.com>

--- .github/scripts/generate-recipe-pack.sh
+++ .github/scripts/generate-recipe-pack.sh
@@ -63,8 +63,8 @@ echo "==> Found ${#RECIPE_DIRS[@]} $RECIPE_TYPE recipe(s)"
 cat > "$OUTPUT_FILE" << EOF
 extension radius
 
-resource $PACK_NAME 'Radius.Core/recipePacks@2025-08-01-preview' = {
-  name: '$PACK_NAME'
+resource PACK_NAME_PLACEHOLDER 'Radius.Core/recipePacks@2025-08-01-preview' = {
+  name: 'PACK_NAME_PLACEHOLDER'
   location: 'global'
   properties: {
     recipes: {
